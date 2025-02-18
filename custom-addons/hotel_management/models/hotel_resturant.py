8bd601b950fb1628db7300d3d5cd8069c73f4310
VivekDudhat <vivekdudhat5@gmail.com>
2025-02-18 14:35:32 +0530

    Changes Resturant

--- custom-addons/hotel_management/models/hotel_resturant.py
+++ custom-addons/hotel_management/models/hotel_resturant.py
@@ -8,12 +8,11 @@ class HotelResturant(models.Model):
     
     _inherits = {'product.product': 'food_id'}
     
-
-    food_id = fields.Many2one('product.product', string="Food item", required=True, ondelete="cascade",
-    domain=[('categ_id.name','=','Food',)]
-    )
-    food_name = fields.Char(
-    string='Food'
-    )
+     
+    room_numner_id = fields.Many2one(string='Room Number',comodel_name='hotel.room.booking',ondelete='restrict',)
+     
+    food_id = fields.Many2one('product.product', string="Food item", required=True, ondelete="cascade",domain=[('categ_id.name','=','Food',)])
+    food_name = fields.Char(string='Food')
+    price = fields.Float(string = "Price",related='food_id.lst_price')
 
 
